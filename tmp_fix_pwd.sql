02c569a14b1e0859ec481d3acfd1eeb7d2648ea3
Liangbiaoyong <2663438224@qq.com>
2026-07-12 16:39:14 +0800

    chore: 清理临时文件

--- tmp_fix_pwd.sql
+++
@@ -1,4 +0,0 @@
--- Fix: update all test account passwords to match admin123
--- Uses the same working BCrypt hash as admin
-UPDATE sys_user SET password = '$2b$10$AgBkzhU4VePwdNIX9qcCRuM//tvoQKV6bh/mHV9P3FmiQ7Zkmztni'
-WHERE username IN ('labadmin', 'student01', 'teacher01');
